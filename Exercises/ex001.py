4ca11eb83e3266e681e592ecd97f63988e3720e5
ysamd1 <161851962+ysamd1@users.noreply.github.com>
2025-03-15 14:10:51 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,10 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:220543009
+# Your Name and Surname:Yaşam Doğa Çevik
+poem = """Twinkle, twinkle, little star,
+    How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+    How I wonder what you are"""
+
+print(poem)
